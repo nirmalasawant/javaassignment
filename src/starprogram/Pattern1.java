a72705a60f3b785a311c73955f6cd691f6603b6a
Admin <Admin@DESKTOP-LVMOHEN>
2023-04-01 13:12:31 +0530

    Pattern1

---
+++ src/starprogram/Pattern1.java
@@ -0,0 +1,21 @@
+package starprogram;
+
+public class Pattern1 {
+	
+	public static void main(String[] args) {
+		
+		int i,j,row=5;
+		
+		for(i=0;i<5;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+			
+	}
+
+}
