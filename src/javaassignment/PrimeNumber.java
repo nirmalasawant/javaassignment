81b5bafbf341d190e2667ae7af62d28193d3d4d6
Admin <Admin@DESKTOP-LVMOHEN>
2023-04-01 12:25:05 +0530

    Code to get first 20 prime number

---
+++ src/javaassignment/PrimeNumber.java
@@ -0,0 +1,27 @@
+package javaassignment;
+
+public class PrimeNumber {
+	
+	public static void main(String[] args)   
+	{  
+		int ct=0,n=0,i=1,j=1;  
+		while(n<20)  
+		{  
+			j=1;  
+			ct=0;  
+			while(j<=i)  
+			{  
+				if(i%j==0)  
+					ct++;  
+				j++;   
+			}  
+			if(ct==2)  
+			{  
+				System.out.printf("%d ",i);  
+				n++;  
+			}  
+			i++;  
+		}  
+	}  
+
+}
