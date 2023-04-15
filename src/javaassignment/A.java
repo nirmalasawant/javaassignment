f8bc6588789ef6c3dc795aed66f9884db09d5089
Admin <Admin@DESKTOP-LVMOHEN>
2023-04-15 09:47:05 +0530

    2 classes A and B in java file

---
+++ src/javaassignment/A.java
@@ -0,0 +1,23 @@
+package javaassignment;
+
+class A{
+
+	  public int add(int i , int j){
+
+	    return i+j;
+
+	  }
+
+	}
+
+	class B{
+
+	  public static void main(String[]  args){
+
+	    A a1 = new A();
+
+	    a1.add(a1.add(10,20),a1.add(30,40));
+
+	  }
+
+	}
